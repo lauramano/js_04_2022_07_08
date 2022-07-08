74ddf06221d4b652ea2c37ecb7821b5fd9cb4e79
laurapras <83669900+laurapras@users.noreply.github.com>
2022-07-08 14:13:33 +0300

    debuger funkcija

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log('Laba, sekmingo darbo');
+// let sum = 0;
+// debugger;
+// for(let i=0; i<6; i++){
+//     sum += i;
+//     console.log(`sis ciklas yra ${i} Bendra suma = ${sum}`)
+// }
+// console.log(`bendra suma po visko =${sum}`);
+
+console.group('continue');
+for(let i=10; i<=20; i++ ){
+    if(i === 15){
+        continue
+    }
+    
+    console.log(`skaicius ${i}`);
+}
+console.groupEnd();
